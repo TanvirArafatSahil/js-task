21d8df27e631f07748cc7920771f297e780e4768
TanvirArafatSahil <sahil32521@gmail.com>
2025-07-31 00:31:40 +0600

    include

---
+++ include.js
@@ -0,0 +1,8 @@
+let books=['bangla', 'english', 'math', 'physics', 'ict']
+if(books.includes('javascript')){
+    console.log('the book is present')
+}
+else{
+    console.log('the book is not present');
+    
+}
